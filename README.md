b056746dd827740526e1f1d1b0d16229eef89b3c
Davi Silva <davisilvaphoto@gmail.com>
2019-05-02 08:10:41 -0300

    vanilla javascript projects

---
+++ README.md
@@ -0,0 +1,6 @@
+## Vanilla Javascript Projects
+
+Some projects with Vanilla Javascritp by Simo Edwin(https://github.com/DevEdwin)
+
+Youtube Playlist:
+(https://www.youtube.com/playlist?list=PLDyQo7g0_nsXlSfuoBpG5Fgz0Qe3IvWnA)
---
+++ typewritting/app.js
@@ -0,0 +1,22 @@
+const texts = ["php7", "html5", "css3", "javascritp"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".typing").textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
+
+  setTimeout(type, 300);
+})();
